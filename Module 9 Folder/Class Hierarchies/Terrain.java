36339efbab6b46c01a5b24fea5dc337c47a048c2
Trisha G <43594876+gtrisha09@users.noreply.github.com>
2021-07-13 12:20:13 -0700

    Create Terrain.java

---
+++ Module 9 Folder/Class Hierarchies/Terrain.java
@@ -0,0 +1,25 @@
+/**
+ * This class defines a basic Terrain.
+ *
+ * @author Trisha Ganesh
+ * @version 7/3/2021
+ */
+
+public class Terrain
+{
+    // instance variables
+    private int length, width;
+
+    // Constructor for objects of class Terrain
+    public Terrain(int l, int w)
+    {
+        // initialize instance variables
+        length = l;
+        width = w;
+    }
+
+    public String getTerrainSize()
+    {
+        return "Land has dimensions " + length + " X " + width;
+    }
+}
